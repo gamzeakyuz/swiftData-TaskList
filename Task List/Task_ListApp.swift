44c234a69691c390678f9e27dfcbc107cc82cd9a
gamzeakyuz <gamz3akyuz@gmail.com>
2025-10-21 15:26:37 +0300

    Initial Commit

---
+++ Task List/Task_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ListApp.swift
+//  Task List
+//
+//  Created by GamzeAkyuz on 21.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Task_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
